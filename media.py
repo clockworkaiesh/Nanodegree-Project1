e76df92f1323cbb5b87a7d0cbe26be52ceca01dd
clockworkaiesh <33953822+clockworkaiesh@users.noreply.github.com>
2018-05-26 17:20:49 +0500

    Update media.py

--- media.py
+++ media.py
@@ -3,7 +3,18 @@ import webbrowser
 
 class Movie():
 
-    """ This is reserved for any kind of documentation on class "Movie()" """
+    """ 
+        class "Movie()"stores all the details about a movie like it's title,
+        story line, poster image and movie's trailer. Every time you will
+        create a new instance of class "Movie()" in entertainment_center.py
+        all of the info you provide there would be saved in class "Movie()"
+        which is inside media.py.
+
+        Class "Movie()" also has a function called
+        "show_trailer()" which will open the trailer for the movie in browser
+        when you click on that movie.
+
+    """
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
 
