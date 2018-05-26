7bd4fbb160a718161a5df34a9880f9f386e2ba28
clockworkaiesh <33953822+clockworkaiesh@users.noreply.github.com>
2018-05-26 17:18:36 +0500

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,60 +1,190 @@
-import fresh_tomatoes
-import media
+import webbrowser
+import os
+import re
 
-lone_survivor = media.Movie("Lone Suvivor",
-                            "Lone Survivor is a 2013 American biographical war thriller film based on the 2007 eponymous non-fiction book",  # NOQA
-                            "https://movieposters2.com/images/1301381-b.jpg",
-                            "https://youtu.be/yoLFk4JK_RM")
+                # Styles and scripting for the page
+
+main_page_head = \
+    '''
+<head>
+    <meta charset="utf-8">
+    <title>Fresh Tomatoes!</title>
+
+    <!-- Bootstrap 3 -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <style type="text/css" media="screen">
+        body {
+            padding-top: 80px;
+        }
+        #trailer .modal-dialog {
+            margin-top: 200px;
+            width: 640px;
+            height: 480px;
+        }
+        .hanging-close {
+            position: absolute;
+            top: -12px;
+            right: -12px;
+            z-index: 9001;
+        }
+        #trailer-video {
+            width: 100%;
+            height: 100%;
+        }
+        .movie-tile {
+            margin-bottom: 20px;
+            padding-top: 20px;
+        }
+        .movie-tile:hover {
+            background-color: #E7A07F;
+            cursor: pointer;
+            color: #FEFBFB;
+        }
+        .scale-media {
+            padding-bottom: 56.25%;
+            position: relative;
+        }
+        .scale-media iframe {
+            border: none;
+            height: 100%;
+            position: absolute;
+            width: 100%;
+            left: 0;
+            top: 0;
+            background-color: white;
+        }
+
+        /******  FEW CHANGES MADE IN BOOTSTRAP CSS STARTS FROM HERE  ******/
+
+        .navbar-inverse .navbar-brand {
+        color: #E7A07F;
+        }
+
+    </style>
+
+    <script type="text/javascript" charset="utf-8">
+
+        // Pause the video when the modal is closed
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+            // Remove the src so the player itself gets removed, as this is the only
+            // reliable way to ensure the video stops playing in IE
+            $("#trailer-video-container").empty();
+        });
+        // Start playing the video whenever the trailer modal is opened
+        $(document).on('click', '.movie-tile', function (event) {
+            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+              'id': 'trailer-video',
+              'type': 'text-html',
+              'src': sourceUrl,
+              'frameborder': 0
+            }));
+        });
+        // Animate in the movies when the page loads
+        $(document).ready(function () {
+          $('.movie-tile').hide().first().show("fast", function showNext() {
+            $(this).next("div").show("fast", showNext);
+          });
+        });
+    </script>
+</head>
+'''
+
+                # The main page layout and title bar
+
+main_page_content = \
+    '''
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <!-- Trailer Video Modal -->
+    <div class="modal" id="trailer">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
+            <img src="https://goo.gl/NAUJ93"/>
+          </a>
+          <div class="scale-media" id="trailer-video-container">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Main Page Content -->
+    <div class="container">
+      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      {movie_tiles}
+    </div>
+  </body>
+</html>
+'''
+
+                # A single movie entry html template
+
+movie_tile_content = \
+    '''
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster_image_url}" width="220" height="342">
+    <h2>{movie_title}</h2>
+</div>
+'''
 
 
-adrift = media.Movie("Adrift",
-                     "Story of 2 avid sailors setting out on a journey across the ocean, meets a catastrophic hurricanes in recorded history.",  # NOQA
-                     "https://ia.media-imdb.com/images/M/MV5BMTkxMTI2MjE4OF5BMl5BanBnXkFtZTgwMjIyODQzNTM@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
-                     "https://youtu.be/n9ukI7khQpE")
+def create_movie_tiles_content(movies):
+
+                # The HTML content for this section of the page
+
+    content = ''
+    for movie in movies:
+
+                # Extract the youtube ID from the url
+
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)  # NOQA
+        youtube_id_match = youtube_id_match \
+            or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = \
+            (youtube_id_match.group(0) if youtube_id_match else None)
+
+                # Append the tile for the movie with its content filled in
+
+        content += movie_tile_content.format(
+            movie_title=movie.title,
+            poster_image_url=movie.poster_image_url,
+            trailer_youtube_id=trailer_youtube_id)
+    return content
 
 
-ratatouille = media.Movie("Ratatouille",
-                          "Remy dreams of becoming a great chef, despite being a rat in a definitely rodent-phobic profession.",  # NOQA
-                          "https://i.pinimg.com/564x/02/d9/1f/02d91f8899c03a2bf656e0b197753b7f.jpg",  # NOQA
-                          "https://youtu.be/uVeNEbh3A4U")
+def open_movies_page(movies):
 
+                # Create or overwrite the output file
 
-big_hero_6 = media.Movie("Big Hero 6",
-                         "Determined to uncover the mystery, Hiro transforms his friends into a band of high-tech heroes called Big Hero 6",  # NOQA
-                         "https://imgix.ttcdn.co/i/product/original/0/538386-1967ef50199e4051bac86bd45bab7ccf.jpeg?q=100&auto=format%2Ccompress&w=500",  # NOQA
-                         "https://youtu.be/z3biFxZIJOQ")
+    output_file = open('fresh_tomatoes.html', 'w')
 
+                # Replace the placeholder for the movie tiles with the actual dynamically generated content # NOQA
 
-age_of_adaline = media.Movie("Age Of Adaline",
-                             "Adaline Bowman has miraculously remained a youthful 29 years of age for nearly eight decades, and hides her secret",  # NOQA
-                             "http://broadcast-everywhere.net/wp-content/uploads/2016/02/Matinee-Movie-The-Age-of-Adaline-PWPL-February-4th.jpg",  # NOQA
-                             "https://youtu.be/7UzSekc0LoQ")
+    rendered_content = \
+        main_page_content.format(
+            movie_tiles=create_movie_tiles_content(movies)
+            )
 
+                # Output the file
 
-me_before_you = media.Movie("Me Before You",
-                            "Young and quirky Louisa, is put to the test when she becomes a caregiver for Will Traynoraccident.",  # NOQA
-                            "http://images6.fanpop.com/image/photos/40000000/Me-Before-You-Movie-Poster-movie-trailers-40097762-380-500.jpg",  # NOQA
-                            "https://youtu.be/Eh993__rOxA")
+    output_file.write(main_page_head + rendered_content)
+    output_file.close()
 
+                # open the output file in the browser
 
-kingsman = media.Movie("Kingsman: The Golden Circle",
-                       "It's James Bond On Laughing Gas.",
-                       "https://20ui41tp7v127j03rcnp97oh-wpengine.netdna-ssl.com/wp-content/uploads/2018/04/sabado_kingsman.jpg",  # NOQA
-                       "https://youtu.be/6Nxc-3WpMbg")
-
-
-mission_impossible = media.Movie("Mission Impossible: Rogue Nation",
-                                 "With the IMF now disbanded and Ethan Hunt out in the cold, a new threat - called the Syndicate - soon emerges",  # NOQA
-                                 "https://images-na.ssl-images-amazon.com/images/I/91PAjeQr62L._SY445_.jpg",  # NOQA
-                                 "https://youtu.be/pXwaKB7YOjw")
-
-
-camp_xray = media.Movie("Camp X Ray",
-                        "A female guard at Guantanamo Bay forms an unlikely friendship with one of the facility's longtime detainees.",  # NOQA
-                        "https://i.pinimg.com/originals/12/bb/f3/12bbf3b84f216643bf5a836b8f13b9f6.jpg",  # NOQA
-                        "https://youtu.be/V602GMSSo0Y")  # NOQA
-
-
-movies = [lone_survivor, adrift, ratatouille, big_hero_6, age_of_adaline, me_before_you, kingsman, mission_impossible, camp_xray]  # NOQA
-
-fresh_tomatoes.open_movies_page(movies)
+    url = os.path.abspath(output_file.name)
+    webbrowser.open('file://' + url, new=2)  # open in a new tab, if possible
